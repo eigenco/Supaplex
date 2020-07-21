fc29d9cbe84ced5e04dc14dc23452839ed1cb25f
eigenco <42321684+eigenco@users.noreply.github.com>
2020-07-21 10:38:32 +0300

    Add files via upload

---
+++ blaster.asm
@@ -0,0 +1,150 @@
+	push    ax
+	push    bx
+	push    cx
+	push    dx
+
+	cmp     ax, 0
+	jne     skip0
+	mov     cx, explosion
+	mov     di, infotron - explosion - 1
+	jmp     short start
+skip0:
+	cmp     ax, 1
+	jne     skip1
+	mov     cx, infotron
+	mov     di, push - infotron - 1
+	jmp     short start
+skip1:
+	cmp     ax, 2
+	jne     skip2
+	mov     cx, push
+	mov     di, zonk - push - 1
+	jmp     short start
+skip2:
+	cmp     ax, 3
+	jne     skip3
+	mov     cx, zonk
+	mov     di, bug - zonk - 1
+	jmp     short start
+skip3:
+	cmp     ax, 4
+	jne     skip4
+	mov     cx, bug
+	mov     di, base - bug - 1
+	jmp     short start
+skip4:
+	cmp     ax, 5
+	jne     skip5
+	mov     cx, base
+	mov     di, exit - base - 1
+	jmp     short start
+skip5:
+	cmp     ax, 6
+	jne     skip6
+	mov     cx, exit
+	mov     di, end - exit - 1
+	jmp     short start
+skip6:
+	jmp     skip
+start:
+	mov     dx, 0x0a               ; DMA: write  mask register
+	mov     al, 15                 ; channel 1 disabled
+	out     dx, al
+
+	mov     dx, 0x0c               ; DMA: clear byte pointer flip-flop
+	mov     al, 0
+	out     dx, al
+
+	mov     dx, 0x0b
+	mov     al, 0x49               ; single-cycle playback on channel 1
+	out     dx, al
+
+	mov     bx, cs
+	shl     bx, 4
+	add     bx, cx                 ; offset
+
+	mov     dx, 0x02               ; DMA: channel 1 address
+	mov     al, bl
+	out     dx, al
+	mov     al, bh
+	out     dx, al
+
+	mov     ax, cs
+	mov     bx, cx
+	shr     bx, 4
+	add     bx, ax
+	shr     bx, 12                 ; DMA: page in bx
+
+	mov     dx, 0x83               ; DMA: channel 1 page
+	mov     al, bl
+	out     dx, al
+
+	mov     bx, di
+	mov     dx, 0x03               ; DMA channel 1 count
+	mov     al, bl
+	out     dx, al
+	mov     al, bh
+	out     dx, al
+
+	mov     dx, 0x0a
+	mov     al, 1                  ; DMA 1 channel enabled
+	out     dx, al
+
+	mov     dx, 0x22c              ; sound blaster (A220) DSP write data
+wait0:
+	in      al, dx                 ; wait for DSP not needed in DOSBOX
+	or      al, al
+	js      wait0
+	mov     al, 0x40               ; sample rate
+	out     dx, al
+
+wait1:
+	in      al, dx
+	or      al, al
+	js      wait1
+	mov     al, 256 - 1000000/8333
+	out     dx, al
+
+wait2:
+	in      al, dx
+	or      al, al
+	js      wait2
+	mov     al, 0x14               ; 8-bit PCM output
+	out     dx, al
+
+wait3:
+	in      al, dx
+	or      al, al
+	js      wait3
+	mov     al, bl                 ; lo(size)
+	out     dx, al
+
+wait4:
+	in      al, dx
+	or      al, al
+	js      wait4
+	mov     al, bh                 ; hi(size)
+	out     dx, al
+skip:
+	pop     dx
+	pop     cx
+	pop     bx
+	pop     ax
+	iret
+state:
+	db      0
+explosion:
+	incbin "explode.raw"
+infotron:
+	incbin "infotron.raw"
+push:
+	incbin "push.raw"
+zonk:
+	incbin "zonk.raw"
+bug:
+	incbin "bug.raw"
+base:
+	incbin "base.raw"
+exit:
+	incbin "exit.raw"
+end:
---
+++ supaplex.asm
@@ -0,0 +1,124 @@
+	cpu     8086
+
+exe_header:
+
+	db "MZ"
+	dw 64                ; number of bytes in the last 512-byte page
+	dw 711               ; total number of 512-byte pages
+	dw 29                ; number of relocations
+	dw 9                 ; (exe_header + reloc_table in bytes) / 16
+	dw 32                ; minimum memory paragraphs in addition to file size
+	dw 65535             ; maximum paragraphs
+	dw 0x587B            ; initial SS
+	dw 0x0400            ; initial SP
+	dw 0
+	dw 0x0000            ; initial IP
+	dw 224288/16         ; initial CS
+	dw reloc_table       ; relocation table offset
+	dw 0
+
+reloc_table:
+
+	dw 0x6C21
+	dw 0x3000
+
+	dw 0x6EB6
+	dw 0x3000
+
+	dw 0x6EDD
+	dw 0x3000
+
+	dw 0x6F2E
+	dw 0x3000
+
+	dw 0x6FC9
+	dw 0x3000
+
+	dw 0x7017
+	dw 0x3000
+
+	dw 0x71AD
+	dw 0x3000
+
+	dw 0x75DA
+	dw 0x3000
+
+	dw 0x7788
+	dw 0x3000
+
+	dw 0x77CF
+	dw 0x3000
+
+	dw 0x8374
+	dw 0x3000
+
+	dw 0x89A8
+	dw 0x3000
+
+	dw 0x9A36
+	dw 0x3000
+
+	dw 0x9FFA
+	dw 0x3000
+
+	dw 0xA060
+	dw 0x3000
+
+	dw 0xB2CC
+	dw 0x3000
+
+	dw 0xB33B
+	dw 0x3000
+
+	dw 0xB3AA
+	dw 0x3000
+
+	dw 0xB40F
+	dw 0x3000
+
+	dw 0xC44B
+	dw 0x3000
+
+	dw 0xC458
+	dw 0x3000
+
+	dw 0xC5EA
+	dw 0x3000
+
+	dw 0xC61F
+	dw 0x3000
+
+	dw 0xEF44
+	dw 0x3000
+
+	dw 0xEFD1
+	dw 0x3000
+
+	dw 0xF012
+	dw 0x3000
+
+	dw 0xF066
+	dw 0x3000
+
+	dw 0xF094
+	dw 0x3000
+
+	dw 0xF130
+	dw 0x3000
+
+before:
+	resb 224288
+
+code:
+	%include "code.asm"
+
+	times 0x3f690 - ($-$$) db 0
+
+data:
+	incbin "data.bin"
+
+after:
+	resb 45912
+
+stack:
+	resb 400h
